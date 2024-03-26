91a56c348b16d8942f276e6491a4f04b54d1f29e
andrea1932734 <andreacarovelli@gmail.com>
2024-03-26 09:26:11 +0000

    quarto

--- es_7_Caro_Mina/src/app/first-component/first-component.component.ts
+++ es_7_Caro_Mina/src/app/first-component/first-component.component.ts
@@ -11,17 +11,34 @@ import { Component } from '@angular/core';
 
 export class FirstComponentComponent {
  pokemon = [
-  {nome: "Anna", numero: "000"},
+  {nome: "Anna", numero: "000", controllo: false},
 
-  {nome: "Bob", numero: "111"},
+  {nome: "Bob", numero: "111",controllo: false},
 
-  {nome: "Charlie", numero: "222"},
+  {nome: "Charlie", numero: "222",controllo: false},
 
-  {nome: "Dana", numero: "333"},
+  {nome: "Dana", numero: "333",controllo: false},
 
-  {nome: "Dana", numero: "333"},
+  {nome: "AB", numero: "333",controllo: false},
 
-  {nome: "Dana", numero: "333"},
+  {nome: "CD", numero: "333",controllo: false},
 
  ]
+
+bottone(pokemon:{nome: string}){
+  if (pokemon.nome == "Anna"){
+    this.pokemon[0].controllo = true
+  }else if (pokemon.nome == "Bob"){
+    this.pokemon[1].controllo = true
+  }else if (pokemon.nome == "Charlie"){
+    this.pokemon[2].controllo = true
+  }else if (pokemon.nome == "Dana"){
+    this.pokemon[3].controllo = true
+  }else if (pokemon.nome == "AB"){
+    this.pokemon[4].controllo = true
+  }else if (pokemon.nome == "CD"){
+    this.pokemon[5].controllo = true
+  }
+}
+
 }
