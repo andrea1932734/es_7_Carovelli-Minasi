c64a3632032378f4c5234d8e5fb93e2c32eba5eb
andrea1932734 <andreacarovelli@gmail.com>
2024-03-26 08:46:09 +0000

    uno

---
+++ es_7_Caro_Mina/src/app/first-component/first-component.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-component.component.html',
+  styleUrl: './first-component.component.css'
+})
+
+export class FirstComponentComponent {
+ pokemon = [
+  {nome: "Anna", numero: "000"},
+
+  {nome: "Bob", numero: "111"},
+
+  {nome: "Charlie", numero: "222"},
+
+  {nome: "Dana", numero: "333"},
+
+ ]
+}
